3fdbbbfa66c198565d8be4f75dd83307bc56e1fa
egem <pierenriquez5@gmail.com>
2025-01-12 14:29:06 -0500

    add maps niños3

--- utils/cache_handler.py
+++ utils/cache_handler.py
@@ -2,7 +2,7 @@ import asyncio
 import streamlit as st
 import pandas as pd 
 
-#@st.cache_data(ttl=1200)
+@st.cache_data(ttl=1200)
 def fetch_vd_childs():
     
     return pd.read_parquet('https://storage.googleapis.com/data-c1-vd-25/actividad_nino.parquet', engine='pyarrow')
