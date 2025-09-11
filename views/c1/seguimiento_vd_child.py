e69f85ca2ef537fd149e8093629f4c8174d28aee
Egem97 <pierenriquez5@gmail.com>
2025-09-11 07:10:20 -0500

    fixed sg

--- views/c1/seguimiento_vd_child.py
+++ views/c1/seguimiento_vd_child.py
@@ -94,6 +94,7 @@ def visitas_ninos_dashboard():
                 
                 datos_ninos_df = fix_data_childs(datos_ninos_df)
                 seg_nominal_df = load_seg_nominal_data(select_mes)
+                st.dataframe(seg_nominal_df)
                 #st.dataframe(seg_nominal_df)
                 #seg_nominal_test = seg_nominal_df.copy()
                 
