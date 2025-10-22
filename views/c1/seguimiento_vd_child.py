1ced31a2bf2c93fb437e379ea0b989bf2032bab8
Egem97 <pierenriquez5@gmail.com>
2025-10-22 08:20:02 -0500

    wws

--- views/c1/seguimiento_vd_child.py
+++ views/c1/seguimiento_vd_child.py
@@ -40,14 +40,14 @@ def visitas_ninos_dashboard():
                     datos_ninos_df = pd.read_parquet('datos_niños.parquet', engine='pyarrow')
                     return actvd_df, carga_df, padron_df, datos_ninos_df
                 
-                @st.cache_data(show_spinner="Cargando datos...")  # ,ttl=600
+                #@st.cache_data(show_spinner="Cargando datos...")  # ,ttl=600
                 def load_seg_nominal_data(select_mes):
                     sn_month = {
                             "Jun": "11nk2Z1DmVKaXthy_TRsYK8wYzQ_BW8sdcRSXGxuq4Zo",
                             "Jul": "11nk2Z1DmVKaXthy_TRsYK8wYzQ_BW8sdcRSXGxuq4Zo",
                             "Ago": "1-jE3mNODE9UXj3dN9G9ae5WLl_Kyow96gWxcB0pOAxo",
                             "Set": "1-jE3mNODE9UXj3dN9G9ae5WLl_Kyow96gWxcB0pOAxo",
-                            "Oct": "1VUARWulWk039rov4fsyM8NJRNd-zWRHyKYhG9tHBefI",
+                            "Oct": "1-jE3mNODE9UXj3dN9G9ae5WLl_Kyow96gWxcB0pOAxo",
                             "Nov": "1XgsYbeYeX9nwyz7jj2PHBYiGcxZcXb9HIiw757XLCcQ",
                             "Dic": "1XgsYbeYeX9nwyz7jj2PHBYiGcxZcXb9HIiw757XLCcQ",
                     }
@@ -95,7 +95,7 @@ def visitas_ninos_dashboard():
                 datos_ninos_df = fix_data_childs(datos_ninos_df)
 
                 seg_nominal_df = load_seg_nominal_data(select_mes)
-                
+                #st.dataframe(seg_nominal_df)
                 #st.dataframe(seg_nominal_df)
                 #seg_nominal_test = seg_nominal_df.copy()
                 
