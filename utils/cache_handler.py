31d195636c326d7a3676993dc00ab747536c700a
Egem97 <pierenriquez5@gmail.com>
2025-11-25 06:50:37 -0500

    2511

--- utils/cache_handler.py
+++ utils/cache_handler.py
@@ -13,18 +13,18 @@ def fetch_vd_childs():
 def fetch_carga_childs():
     
     #return pd.read_parquet(f'./data/backups/carga_nino.parquet', engine='pyarrow')
-    return pd.read_parquet(f'http://72.60.252.104:7001/data/carga_nino.parquet', engine='pyarrow')
+    return pd.read_parquet(f'./data/backups/carga_nino.parquet', engine='pyarrow')
 
 @st.cache_data(ttl=DEELAY_DATA,show_spinner="Cargando datos vd gestantes...")
 def fetch_vd_gestantes():
-    return pd.read_parquet(f'http://72.60.252.104:7001/data/actividad_gestante.parquet', engine='pyarrow')
+    return pd.read_parquet(f'./data/backups/actividad_gestante.parquet', engine='pyarrow')
 
 @st.cache_data(ttl=DEELAY_DATA,show_spinner="Cargando datos gestantes...")
 def fetch_carga_gestantes():
-    return pd.read_parquet(f'http://72.60.252.104:7001/data/carga_gestantes.parquet', engine='pyarrow')
+    return pd.read_parquet(f'./data/backups/carga_gestantes.parquet', engine='pyarrow')
 
 @st.cache_data(ttl=DEELAY_DATA,show_spinner="Cargando datos padron...")
 def fetch_padron():
-    return pd.read_parquet(f'http://72.60.252.104:7001/data/padron.parquet', engine='pyarrow')
+    return pd.read_parquet(f'./data/backups/padron.parquet', engine='pyarrow')
 
 
